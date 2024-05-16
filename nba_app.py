dcc2ece2961105e4fc99ecadac10c81ae3287cdb
Simone Binotto <156416395+SBino01@users.noreply.github.com>
2024-05-16 21:50:18 +0200

    Update nba_app.py

--- nba_app.py
+++ nba_app.py
@@ -5,12 +5,8 @@ def main():
     
     st.write("""
     Benvenuto nella nostra pagina Streamlit! 
-    Qui di seguito, troverai un testo e un'immagine.
+    Qui di seguito, troverai un testo.
     """)
-
-    # Aggiungi un'immagine dalla directory locale
-    st.image("image.jpg", caption="Immagine di esempio")
-
     st.write("""
     Grazie per aver visitato la nostra pagina. 
     Speriamo ti sia divertito!
