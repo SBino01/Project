2cdfe35f7a3031f9c769bda4a69b9dcb81264265
Simone Binotto <156416395+SBino01@users.noreply.github.com>
2024-05-16 21:49:19 +0200

    Update nba_app.py

--- nba_app.py
+++ nba_app.py
@@ -1,37 +1,20 @@
-!pip install streamlit
-!pip install seabon
-!pip install matplotlib
-
 import streamlit as st
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 def main():
-    st.title("App Streamlit con Grafico Interattivo")
+    st.title("Pagina Semplice con Streamlit")
+    
+    st.write("""
+    Benvenuto nella nostra pagina Streamlit! 
+    Qui di seguito, troverai un testo e un'immagine.
+    """)
 
-    # Carica dati di esempio
-    df = sns.load_dataset('iris')
+    # Aggiungi un'immagine dalla directory locale
+    st.image("image.jpg", caption="Immagine di esempio")
 
-    # Opzioni per selezionare il tipo di grafico
-    chart_type = st.sidebar.selectbox("Seleziona il tipo di grafico", 
-                                      ["Grafico a barre", "Grafico a dispersione"])
-
-    if chart_type == "Grafico a barre":
-        # Calcola la media dei valori numerici per ogni specie
-        avg_data = df.groupby('species').mean()
-
-        # Visualizza il grafico a barre
-        st.bar_chart(avg_data)
-
-    elif chart_type == "Grafico a dispersione":
-        # Seleziona due colonne per il grafico a dispersione
-        x_col = st.sidebar.selectbox("Seleziona la colonna x", df.columns[:-1])
-        y_col = st.sidebar.selectbox("Seleziona la colonna y", df.columns[:-1])
-
-        # Visualizza il grafico a dispersione
-        plt.figure(figsize=(8, 6))
-        sns.scatterplot(data=df, x=x_col, y=y_col, hue='species')
-        st.pyplot()
+    st.write("""
+    Grazie per aver visitato la nostra pagina. 
+    Speriamo ti sia divertito!
+    """)
 
 if __name__ == "__main__":
     main()
