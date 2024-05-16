05ebd3b7f2744f9f460fa3f14d7c3ca4910c658b
Simone Binotto <156416395+SBino01@users.noreply.github.com>
2024-05-16 21:52:23 +0200

    Update nba_app.py

--- nba_app.py
+++ nba_app.py
@@ -1,15 +1,11 @@
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
 
 def plot_function(a, b, c):
     x = np.linspace(-10, 10, 400)
     y = a * x**2 + b * x + c
-    plt.plot(x, y)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('Grafico della funzione quadratica')
-    st.pyplot()
+    data = np.column_stack((x, y))
+    st.line_chart(data, use_container_width=True)
 
 def main():
     st.title('Pagina Streamlit con Grafico Interattivo')
